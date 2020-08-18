6cd0e334a3e875c5a2c3c1d901d23ae1a77970d5
juan gonzalez <juaniiiii1025@gmail.com>
2020-08-18 18:40:06 -0300

    primer commit

---
+++ cuarto proyecto/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cuarto_proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int aleatorio, introducido;
+                int restantes = 13;
+                Random generador = new Random();
+                aleatorio = generador.Next(1, 1001);
+
+                Console.WriteLine("Te quedan {0} intentos", restantes);
+                Console.Write("Introduce un número: ");
+                introducido = Convert.ToInt32(Console.ReadLine());
+
+                //Bucle que se repite hasta que acierte o se quede sin intentos
+                while ((introducido != aleatorio) && (restantes > 1))
+                {
+                    restantes = restantes - 1;
+                    if (introducido < aleatorio)
+                        Console.WriteLine("Te has quedado corto");
+                    if (introducido > aleatorio)
+                        Console.WriteLine("Te has pasado");
+
+                    Console.WriteLine("Te quedan {0} intentos", restantes);
+                    Console.Write("Introduce un número: ");
+                    introducido = Convert.ToInt32(Console.ReadLine());
+                }
+                //Comprobar si gana o pierde
+                if (introducido == aleatorio)
+                    Console.WriteLine("Has ganado!");
+                else
+                {
+                    Console.WriteLine("Has perdido!");
+                    Console.WriteLine("Era el {0}", aleatorio);
+                }
+
+            Console.ReadKey();
+
+            }
+        }
+    }
